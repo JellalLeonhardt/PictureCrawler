90034630012813e921e14840ac6a6c00e99b750d
bandwagonhost <1029458906@qq.com>
2018-12-03 04:32:43 -0500

    finish xiuren_org

--- xiuren_org.py
+++ xiuren_org.py
@@ -3,39 +3,34 @@ import requests
 import os
 import time
 from lxml import html
-section = input("target:")
+section = raw_input("target:")
 a = int(input("start:"))
 b = int(input("end:"))
 send_headers = {
-    'Host': 'www.meizitu.com',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Connection': 'keep-alive'
 }
 for num in range(a, b + 1, 1):
-	url = 'http://www.xiuren.org/' + section + '-{0:03d}.html'.format(num)
-	req = urllib2.Request(url, headers=send_headers)
-	r = urllib2.urlopen(req)
+    url = 'http://www.xiuren.org/' + section + '-{0:03d}.html'.format(num)
+    req = urllib2.Request(url, headers=send_headers)
+    try:
+    	r = urllib2.urlopen(req)
+    except Exception as e:
+	print('Error:', e)
+	break
     source = r.read()
-	if r.getcode() == 404:
-		break
-    title = html.fromstring(source.decode('utf-8')).xpath('//div[@id="title"]/h1/text()')
+    target = html.fromstring(source.decode('utf-8'))
+    title = target.xpath('//div[@id="title"]/h1/text()')[0]
     print title
-    d = 0
-    url = 'http://www.xiuren.org/' + section + '/{0:03d}/'.format(num)
-	fname = '/home/pictures/' + title
+    purl = target.xpath('//span[@class="photoThum"]/a/@href')
+    fname = '/home/pictures/' + title
     os.makedirs(fname)
-    while 1:
-		d = d + 1
-		url = url + '{0:04d}.jpg'.format(d)
-        print url
+    d = 0
+    for jpg_url in purl:
+	print jpg_url
+	d = d + 1
         name = fname+('/{0:04d}.jpg'.format(d))
-        print name
-		try:
-			with open(name, "wb") as jpg:
-				jpg.write(requests.get(c).content)
-				time.sleep(0.1)
-   		except Exception as e:
-        	print('Error:', e)
-			break
+        with open(name, "wb") as jpg:
+            jpg.write(requests.get(jpg_url).content)
     print('finishi!', num)
