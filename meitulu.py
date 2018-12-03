a1295da6004ac943eb10a6031319fcd42a613bdc
ASUS laptop <1029458906@qq.com>
2018-12-03 20:33:51 +0800

    meizilu

---
+++ meitulu.py
@@ -0,0 +1,33 @@
+import urllib2
+import requests
+import os
+import time
+from lxml import html
+a = int(input("start:"))
+b = int(input("end:"))
+send_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0'
+}
+for num in range(a, b + 1, 1):
+    url = 'http://mtl.ttsqgs.com/images/img/' + '{}/'.format(num)
+    img = 0
+    send_headers['Reffer'] = 'https://www.meitulu.com/item/{}.html'.format(num)
+    req = urllib2.Request(send_headers['Reffer'], headers=send_headers)
+    r = urllib2.urlopen(req)
+    source = r.read()
+    target = html.fromstring(source.decode('utf-8'))
+    title = target.xpath('//div[@class="weizhi"]/h1/text()')[0]
+    fname = '/home/pictures/' + title
+    while 1:
+        img = img + 1
+        img_url = url + '{}.jpg'.format(img)
+        req = urllib2.Request(img_url, headers=send_headers)
+        try:
+            r = urllib2.urlopen(req)
+            name = fname + '/{}.jpg'.format(img)
+			with open(name, "wb") as jpg:
+            	jpg.write(r.read())
+        except Exception as e:
+            print('Error:', e)
+            break
+    print('finishi!', num)
