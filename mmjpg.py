8fe6c9d8a1eafa682d86c5a062089a8f48645326
bandwagonhost <1029458906@qq.com>
2018-12-03 07:20:38 -0500

    mmjpg not test

--- mmjpg.py
+++ mmjpg.py
@@ -3,16 +3,13 @@ import requests
 import os
 import time
 from lxml import html
-a = int(input("start:"))
-b = int(input("end:"))
 year = 2015
 page = 1
 os.makedirs('{}'.format(page))
 while 1:
-	img = 1
+    i=1
     d = 0
-    url = 'http://fm.shiyunjj.com/{}'.format(year) + \
-        '/{}'.format(page) + '/{}.jpg'.format(img)
+    url = 'http://fm.shiyunjj.com/{}'.format(year) + '/{}'.format(page) + '/{}.jpg'.format(i)
     send_headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -22,19 +19,18 @@ while 1:
     req = urllib2.Request(url, headers=send_headers)
     try:
         r = urllib2.urlopen(req)
-		if r.getcode() == 404:
-			if img == 1:
-				print '+1y'
-				year = year + 1
-				img = 1
-				continue
-			print '+1p'
-			page = page + 1
-			os.makedirs('{}'.format(page))
-			img = 1
-			continue
-		r.close()
-
+	if r.getcode() == 404:
+            if i == 1:
+		print '+1y'
+		year = year + 1
+		i=1
+		continue
+	    print '+1p'
+	    page = page + 1
+	    i=1
+	    os.makedirs('{}'.format(page))
+	    continue
+	r.close()
         fname = '/home/pictures/{}'.format(page) + '/{}.jpg'.format(img)
         with open(fname, "wb") as jpg:
             jpg.write(requests.get(url).content)
