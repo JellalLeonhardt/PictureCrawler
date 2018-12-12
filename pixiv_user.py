9919720288426fe2b3d03b60446184193d43448a
ASUS laptop <1029458906@qq.com>
2018-12-12 15:29:02 +0800

    xhr

--- pixiv_user.py
+++ pixiv_user.py
@@ -2,14 +2,22 @@ import urllib2
 import requests
 import os
 import time
+import json
 from lxml import html
 a = int(input("start:"))
 b = int(input("end:"))
 send_headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0'
+    'accept': 'application/json',
+	'accept-encoding': 'gzip, deflate, br',
+	'accept-language': 'zh-CN,zh;q=0.9',
+	'cache-control': 'no-cache',
+	'cookie': '',
+	'pragma': 'no-cache',
+	'referer': 'https://www.pixiv.net/member_illust.php?id=340628',
+	'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
 }
 for num in range(a, b + 1, 1):
-	url = 'https://www.pixiv.net/member_illust.php?id={}'.format(num)
+	url = 'https://www.pixiv.net/ajax/user/{}/profile/all'.format(num)
     req = urllib2.Request(url, headers=send_headers)
     try:
         r = urllib2.urlopen(req)
@@ -17,6 +25,10 @@ for num in range(a, b + 1, 1):
         print('Error:', e)
         break
     source = r.read()
+	images_json = json.loads(source)
+	for image in images_json['body']['illusts']:
+		print image
+	exit()
     target = html.fromstring(source.decode('utf-8'))
     titles = target.xpath('//a[@class="sc-hSdWYo NeoCZ"]/text()')
     purls = target.xpath('//a[@class="sc-hSdWYo NeoCZ"]/@href')
